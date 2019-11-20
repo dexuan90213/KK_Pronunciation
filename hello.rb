57c004f700a6c05d06a2a713773329cd97d91c73
Eden Yang <dexuan90213@gmail.com>
2019-11-20 20:39:09 +0800

    add erb layout

--- hello.rb
+++ hello.rb
@@ -9,11 +9,11 @@ end
 
 get '/yahoodictest' do
 
-  list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
-  result = []
+  @list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
+  @result = []
   
-  sss = Benchmark.measure do
-  list.each do |word|
+  @sss = Benchmark.measure do
+  @list.each do |word|
 
     uri = URI("https://tw.dictionary.search.yahoo.com/search?p=#{word}")
 
@@ -21,10 +21,13 @@ get '/yahoodictest' do
     doc = Nokogiri::HTML(response)
 
     kk = doc.css('div.compList ul li span.fz-14').first.content
-    result << (kk.delete 'KK')
+    @result << (kk.delete 'KK')
   end
   end
 
-  "<h3>單字數量：#{list.count}</h3><br> <p>#{list}</p><br>#{result} ----- #{sss}"
+  erb :index, layout: :my_layout
+
+  # "<h3>單字數量：#{list.count}</h3><br> <p>#{list}</p><br>" + 
+    # "<ul><% re</ul>" +  "----- #{sss}"
 
 end
