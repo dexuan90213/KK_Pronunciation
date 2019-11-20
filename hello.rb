cb51a2ae4a08dca7216990350fe2785cecf506d5
Eden Yang <dexuan90213@gmail.com>
2019-11-20 20:39:09 +0800

    add erb layout

--- hello.rb
+++ hello.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'net/http'
 require 'nokogiri'
 require 'benchmark'
+require 'typhoeus'
 
 get '/' do
   "Hello Sinatra"
@@ -9,11 +10,11 @@ end
 
 get '/yahoodictest' do
 
-  list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
-  result = []
+  @list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
+  @result = []
   
-  sss = Benchmark.measure do
-  list.each do |word|
+  @sss = Benchmark.measure do
+  @list.each do |word|
 
     uri = URI("https://tw.dictionary.search.yahoo.com/search?p=#{word}")
 
@@ -21,10 +22,30 @@ get '/yahoodictest' do
     doc = Nokogiri::HTML(response)
 
     kk = doc.css('div.compList ul li span.fz-14').first.content
-    result << (kk.delete 'KK')
+    @result << (kk.delete 'KK')
   end
   end
 
-  "<h3>單字數量：#{list.count}</h3><br> <p>#{list}</p><br>#{result} ----- #{sss}"
+  erb :index, layout: :my_layout
 
+  # "<h3>單字數量：#{list.count}</h3><br> <p>#{list}</p><br>" + 
+    # "<ul><% re</ul>" +  "----- #{sss}"
+
+end
+
+get '/typhoeus' do
+  requests = ddd
+  "#{requests}"
+end
+
+def ddd
+  hydra = Typhoeus::Hydra.new
+  list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
+  requests = list.map { |word| 
+    request = Typhoeus::Request.new("https://tw.dictionary.search.yahoo.com/search?p=#{word}", followlocation: true)
+    hydra.queue(request) 
+    request
+  }
+  hydra.run
+  requests.map { |r| r.response.response_body.force_encoding('UTF-8')  }
 end
