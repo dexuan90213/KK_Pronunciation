a833de835f44eb6ae1201ffc64afe4373de36350
Eden Yang <dexuan90213@gmail.com>
2019-11-20 20:00:09 +0800

    add yahoodictest

--- hello.rb
+++ hello.rb
@@ -7,11 +7,10 @@ get '/' do
   "Hello Sinatra"
 end
 
-get '/:word' do
+get '/yahoodictest' do
 
-  list = Array.new
+  list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape scared still dream eye sound future job act actress actor memorize line enough teach reporter interview businessman fisherman try pose age start begin create company become team different perfect popular share keep right library soon until noon hospital stomachache skip important stomach hurt sick rest late tomorrow fever cough headache diary stay junk food tell regular healthy health decide difficult way cold high from now on)
   result = []
-  list.push(params[:word])
   
   sss = Benchmark.measure do
   list.each do |word|
@@ -26,6 +25,6 @@ get '/:word' do
   end
   end
 
-  "#{result} ----- #{sss}"
+  "<h3>單字數量：#{list.count}</h3><br> <p>#{list}</p><br>#{result} ----- #{sss}"
 
 end
