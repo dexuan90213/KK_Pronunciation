67c3e6f2fdc9c45520c58aabaed68fe25d43e87c
Eden Yang <dexuan90213@gmail.com>
2019-11-21 12:29:16 +0800

    change to typhoeus

--- hello.rb
+++ hello.rb
@@ -33,18 +33,52 @@ get '/yahoodictest' do
 end
 
 get '/typhoeus' do
-  requests = ddd
-  "#{requests}"
+    @list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
+
+    @result = []
+
+  @sss = Benchmark.measure do
+    hydra = Typhoeus::Hydra.new
+    requests = @list.map { |word| 
+      request = Typhoeus::Request.new("https://tw.dictionary.search.yahoo.com/search?p=#{word}", followlocation: true)
+      hydra.queue(request) 
+      request
+    }
+    hydra.run
+    responses = requests.map { |r| r.response.response_body } 
+
+    
+    responses.each do |response|
+      doc = Nokogiri::HTML(response)
+
+      kk = doc.css('div.compList ul li span.fz-14').first.content
+      @result << (kk.delete 'KK')
+    end
+  end
+
+    erb :index, layout: :my_layout
+
 end
 
 def ddd
-  hydra = Typhoeus::Hydra.new
-  list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
-  requests = list.map { |word| 
-    request = Typhoeus::Request.new("https://tw.dictionary.search.yahoo.com/search?p=#{word}", followlocation: true)
-    hydra.queue(request) 
-    request
-  }
-  hydra.run
-  requests.map { |r| r.response.response_body.force_encoding('UTF-8')  }
+  @sss = Benchmark.measure do
+    hydra = Typhoeus::Hydra.new
+    @list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape)
+    requests = @list.map { |word| 
+      request = Typhoeus::Request.new("https://tw.dictionary.search.yahoo.com/search?p=#{word}", followlocation: true)
+      hydra.queue(request) 
+      request
+    }
+    hydra.run
+    responses = requests.map { |r| r.response.response_body } 
+
+    @reault = []
+    
+    responses.each do |response|
+      doc = Nokogiri::HTML(response)
+
+      kk = doc.css('div.compList ul li span.fz-14').first.content
+      @result << (kk.delete 'KK')
+    end
+  end
 end
