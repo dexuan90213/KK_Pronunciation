a9aad72a0f9f55a036952f88973a5e63a9298597
Eden Yang <dexuan90213@gmail.com>
2019-11-21 12:38:22 +0800

    add w86

--- hello.rb
+++ hello.rb
@@ -60,6 +60,39 @@ get '/typhoeus' do
 
 end
 
+
+
+get '/w86' do
+
+ @list =  %w(video find honey stop half just quarter lie sleepwalk housework sweep mop  wipe window  mat someone laugh move body shape scared still dream eye sound future job act actress actor memorize line enough teach reporter interview businessman fisherman try pose age start begin create company become team different perfect popular share keep right library soon until noon hospital stomachache skip important stomach hurt sick rest late tomorrow fever cough headache diary stay junk food tell regular healthy health decide difficult way cold high from now on)
+    @result = []
+
+  @sss = Benchmark.measure do
+    hydra = Typhoeus::Hydra.new
+    requests = @list.map { |word| 
+      request = Typhoeus::Request.new("https://tw.dictionary.search.yahoo.com/search?p=#{word}", followlocation: true)
+      hydra.queue(request) 
+      request
+    }
+    hydra.run
+    responses = requests.map { |r| r.response.response_body } 
+
+    
+    responses.each do |response|
+      doc = Nokogiri::HTML(response)
+
+      kk = doc.css('div.compList ul li span.fz-14').first.content
+      @result << (kk.delete 'KK')
+    end
+  end
+
+    erb :index, layout: :my_layout
+
+end
+
+
+
+
 def ddd
   @sss = Benchmark.measure do
     hydra = Typhoeus::Hydra.new
